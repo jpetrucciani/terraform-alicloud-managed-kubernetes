64c27ad7b7b65c0df8a43554ca69b2b247709fd7
jacobi petrucciani <jacobi.petrucciani@medable.com>
2022-09-19 10:41:25 -0400

    update worker_instance_charge_type

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
 resource "alicloud_cs_managed_kubernetes" "this" {
-  count                 = length(local.vswitch_ids) > 0 ? 1 : 0
-  name                  = local.k8s_name
-  worker_vswitch_ids    = local.vswitch_ids
-  pod_vswitch_ids       = local.pod_vswitch_ids
-  new_nat_gateway       = var.new_vpc == true ? false : var.new_nat_gateway
-  worker_disk_category  = var.worker_disk_category
-  password              = var.ecs_password
-  pod_cidr              = var.k8s_pod_cidr
-  service_cidr          = var.k8s_service_cidr
-  slb_internet_enabled  = true
-  install_cloud_monitor = true
-  version               = var.kubernetes_version
-  worker_instance_types = var.worker_instance_types
-  worker_number         = var.worker_number
+  count                       = length(local.vswitch_ids) > 0 ? 1 : 0
+  name                        = local.k8s_name
+  worker_vswitch_ids          = local.vswitch_ids
+  pod_vswitch_ids             = local.pod_vswitch_ids
+  new_nat_gateway             = var.new_vpc == true ? false : var.new_nat_gateway
+  worker_disk_category        = var.worker_disk_category
+  password                    = var.ecs_password
+  pod_cidr                    = var.k8s_pod_cidr
+  service_cidr                = var.k8s_service_cidr
+  slb_internet_enabled        = true
+  install_cloud_monitor       = true
+  version                     = var.kubernetes_version
+  worker_instance_types       = var.worker_instance_types
+  worker_instance_charge_type = "PostPaid"
+  worker_number               = var.worker_number
   dynamic "addons" {
     for_each = var.cluster_addons
     content {
